fb5825e2fb1babe9c6683d3a2b395ccedb88f899
shoryakhyalia <154425600+shoryakhyalia@users.noreply.github.com>
2024-01-25 20:00:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function tablet()
+{
+    window.location = "index2.html";    
+}
+function SOFTGEL()
+{
+    window.location = "index3.html";    
+}
+function SYRUPS()
+{
+    window.location = "index4.html";    
+}
+function DRYSYRUPS()
+{
+    window.location = "index5.html";    
+}
+function DROPS()
+{
+    window.location = "index6.html";    
+}
+function POWDER()
+{
+    window.location = "index7.html";    
+}
+function INJECTABLE()
+{
+    window.location = "index8.html";    
+}
+function EYEDROPS()
+{
+    window.location = "index9.html";    
+}
+function OINTMENT()
+{
+    window.location = "index0.html";    
+}
+function DENTAL()
+{
+    window.location = "indexx1.html";    
+}
+function HERBAL()
+{
+    window.location = "indexx2.html";    
+}
+function index()
+{
+    window.location = "index.html";    
+}
